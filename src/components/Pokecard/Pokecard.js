5106f0be45dc989cac2f2687d7af0e99f546dc68
monica <mogimartinez@gmail.com>
2019-07-22 17:26:23 -0500

    added pokemon json file

--- src/components/Pokecard/Pokecard.js
+++ src/components/Pokecard/Pokecard.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import React from 'react';
 
 class Pokecard extends React.Component {
     render(){
         return (
+            <div className="pokeDetails">
 
-            
+            </div>
         )
     }
 }
+
+export default Pokecard;
--- src/components/Pokedex/Pokedex.js
+++ src/components/Pokedex/Pokedex.js
@@ -3,9 +3,5 @@ import React from 'react';
 class Pokedex extends React.Component {
     render() {
         return ( 
-           <div className="pokeDetails">
-               
-           </div> 
-        )
-    }
+       
 }
