59ed9c1f21934f66211b28a655f00176b892426d
monica <mogimartinez@gmail.com>
2019-07-22 16:25:48 -0500

    initial component files

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    render(){
+        return (
+            <nav class="navbar navbar-light bg-light">
+                <span class="navbar-brand mb-0 h1">Pokedex</span>
+            </nav> 
+        )
+    }
+}
+
+export default Navbar;
---
+++ src/components/Pokedex/Pokedex.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Pokedex extends React.Component {
+    render(){
+        return (
+
+            
+        )
+    }
+}
