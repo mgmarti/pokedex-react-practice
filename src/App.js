50cc9b4b7f9192029a60ffc9993951cb5cf8b84a
monica <mogimartinez@gmail.com>
2019-07-22 19:17:56 -0500

    displaying all pokemon

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Pokecard from './components/Pokecard/Pokecard';
 import pokemon from './pokemon.json';
+import Pokedex from './components/Pokedex/Pokedex';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Pokecard 
-      img={pokemon[0].img}
-      name={pokemon[0].name} 
-      />
-      
+
+      <div className="pokemon">
+        <Pokedex>
+          {pokemon.map(pokemon => (
+              <Pokecard 
+              img={pokemon.img}
+              name={pokemon.name} 
+              />
+          ))}
+        </Pokedex>
+      </div>
+
+    
     </div>
   );
 }
--- src/components/Pokedex/Pokedex.js
+++ src/components/Pokedex/Pokedex.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-class Pokedex extends React.Component {
+class Pokedex extends Component {
     render(){
         return(
-          
+          <div className="wrapper">
+              {this.props.children}
+          </div>
         )
     }
 }
+
+export default Pokedex;
